3c296e349db5a809678fab51e7e523197e665ef7
kaushik.bora <herock76@gmail.com>
2022-10-15 23:35:47 +0530

    -correction

--- AdvancedModule/src/Stacks/ValidParenthesisAll.java
+++ AdvancedModule/src/Stacks/ValidParenthesisAll.java
@@ -4,35 +4,31 @@ import java.util.Stack;
 
 public class ValidParenthesisAll {
     public static void main(String[] args) {
-        String input="(])";
+        String input="(]}])}";
         System.out.println(isValid(input));
     }
 
     public static boolean isValid(String s){
-        if(s.length()%2!=0)
+        if(s.length() % 2 != 0){
             return false;
-        Stack<Character> stack=new Stack<>();
-        for(char c:s.toCharArray()){
-            if(c == '(' || c == '[' ||c == '{' )
-                stack.push(c);
-            if(c==')'){
-                if(stack.isEmpty())
-                    return false;
-                else if(stack.peek()=='(')
-                    stack.pop();
-            }
-            if(c=='}' ){
-                if(stack.isEmpty())
-                    return false;
-                else if(stack.peek()=='{')
-                    stack.pop();
-            }
+        }
 
-            if(c==']' ){
-                if(stack.isEmpty())
-                    return false;
-                else if(stack.peek()=='[')
-                    stack.pop();
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else if(c == ')' && !stack.isEmpty() && stack.peek() == '(' ){
+                stack.pop();
+            }
+            else if(c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }
+            else{
+                return false; //when cases like }}, )), ([}}]) this will return false
             }
         }
         return stack.isEmpty();
