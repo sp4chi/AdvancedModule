aff6d9430811b8817d98c40dbdb40a1ee2f00cf9
kaushik.bora <herock76@gmail.com>
2022-10-15 21:09:46 +0530

    -valid parenthesis

--- AdvancedModule/src/Stacks/ValidParenthesis.java
+++ AdvancedModule/src/Stacks/ValidParenthesis.java
@@ -1,2 +1,26 @@
-package Stacks;public class ValidParenthesis {
+package Stacks;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean check(String s){
+        Stack<Character> stack= new Stack<>();
+        for(int x=0;x<s.length();x++){
+            if(s.charAt(x) == '(' )
+                stack.push('(');
+            if(s.charAt(x)==')'){
+                if(stack.isEmpty())
+                    return false;
+                else
+                    stack.pop();
+            }
+        }
+        return stack.empty();
+
+    }
+    public static void main(String[] args) {
+        String input="((1+1)/(3*3)";
+        System.out.println(check(input));
+    }
 }
+
