d4b8ac7d4abfb97577c75d840eadecdcf50ee5c7
kaushik.bora <herock76@gmail.com>
2022-10-15 22:40:31 +0530

    -valid parenthesis_all

---
+++ AdvancedModule/src/Stacks/ValidParenthesisAll.java
@@ -0,0 +1,25 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ValidParenthesisAll {
+    public static void main(String[] args) {
+        String input="{([]}";
+        System.out.println(isValid(input));
+    }
+
+    public static boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c == '(' || c == '[' ||c == '{' )
+                stack.push(c);
+            if(c==')' && !stack.isEmpty() && stack.peek()=='(')
+                stack.pop();
+            if(c=='}' && !stack.isEmpty() && stack.peek()=='{')
+                stack.pop();
+            if(c==']' && !stack.isEmpty() && stack.peek()=='[')
+                stack.pop();
+        }
+        return stack.isEmpty();
+    }
+}
