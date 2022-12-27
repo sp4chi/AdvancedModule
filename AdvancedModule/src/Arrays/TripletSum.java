c1d8b345fecb8589468f3c6464da6a32feaab775
sp4chi <coshik@live.com>
2022-12-28 02:08:31 +0530

    TripletSum Problem(google)

---
+++ AdvancedModule/src/Arrays/TripletSum.java
@@ -0,0 +1,37 @@
+package Arrays;
+import java.util.*;
+public class TripletSum {
+    public static void PrintTripletPairs (int[] A,int sum){
+        int len=A.length;
+        Arrays.sort(A);
+//        for (int x:A) {
+//            System.out.print(x+" ");
+//        }
+//        System.out.println();
+
+        int total = 0;
+        for(int i=0;i<len-3;i++){
+            int target=sum-A[i];
+            int j=i+1;
+            int k=len-1;
+            int cnt=0;
+            //[1,2,3,4,6,7,8,10]
+            while(j<k) {
+                if (A[j] + A[k] > target) {
+                    k--;
+                } else if (A[j] + A[k] <= target) {
+                    cnt += (k - j);
+                    j++;
+                   // System.out.println(cnt);
+                }
+            }
+            total+=cnt;
+        }
+        System.out.println("total pairs : "+total);
+    }
+
+    public static void main(String[] args) {
+        int[] A = {10,8,2,3,4,6,7,1};
+        PrintTripletPairs(A,12);
+    }
+}
---
+++ AdvancedModule/src/HeapsAndPriorityQueue/RopeMerge.java
@@ -0,0 +1,4 @@
+package HeapsAndPriorityQueue;
+
+public class RopeMerge {
+}
