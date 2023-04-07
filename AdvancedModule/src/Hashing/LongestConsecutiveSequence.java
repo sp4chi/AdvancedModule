4fc10322538425ada05d49af68ada504c45b6bc7
kaushik.bora <herock76@gmail.com>
2023-04-07 06:41:39 +0530

    -added brute force approach

--- AdvancedModule/src/Hashing/LongestConsecutiveSequence.java
+++ AdvancedModule/src/Hashing/LongestConsecutiveSequence.java
@@ -3,10 +3,37 @@ package Hashing;
 import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
+    //Brute Force
+    public static int findingLongestBrute(int[] A){
+        int N = A.length;
+        int max = 0;
+        //loop to traverse the array
+        for (int value : A) {
+            int temp = value - 1;
+            int k = 0;
+            for (int j = 0; j < N; j++) {
+                if (linearSearch(A, temp) != -1) {
+                    k++;
+                    temp++;
+                }
+            }
+            max = Math.max(max, k);
+        }
+        return max;
+    }
+    public static int linearSearch(int[] A, int target){
+        int N = A.length;
+        for(int i=0;i<N;i++){
+            if(A[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
     //Given an unsorted integer array A of size N.
     //Find the length of the longest set of consecutive elements from array A.
     public static int findingLongestSubsequence(int[] A){
-        int maxSubsequence=0;
+        int maxSubsequence = 0;
         HashSet<Integer> set = new HashSet<>();
         for(int i:A){
             set.add(i);
@@ -26,7 +53,9 @@ public class LongestConsecutiveSequence {
     }
 
     public static void main(String[] args) {
-        int[] A={100,4,200,1,3,2};
-        System.out.println(findingLongestSubsequence(A));
+        //int[] A={100,4,200,1,3,2};
+        //int[] A={2,1,4};
+        int[] A={11,21,13,14,15,12,10,2, 1};
+        System.out.println(findingLongestBrute(A));
     }
 }
