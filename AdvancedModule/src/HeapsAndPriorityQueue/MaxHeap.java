50b1aa10fde0a6fcd0ed4e70ae84329b4801109c
kaushik.bora <herock76@gmail.com>
2023-04-12 22:42:54 +0530

    -minor fixes

--- AdvancedModule/src/HeapsAndPriorityQueue/MaxHeap.java
+++ AdvancedModule/src/HeapsAndPriorityQueue/MaxHeap.java
@@ -1,32 +1,33 @@
 package HeapsAndPriorityQueue;
 
-import java.util.Collections;
-
 public class MaxHeap {
     int[] arr;
     int capacity;
     int size;
-    MaxHeap(int maxLimit){
+
+    MaxHeap(int maxLimit) {
         capacity = maxLimit;
         size = 0;
-        arr = new int[capacity+1];
+        arr = new int[capacity + 1];
     }
-    void swap(int a,int b){
-        int temp=a;
-        a=b;
-        b=temp;
+
+    void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
     }
-    void insert(int data){
+
+    void insert(int data) {
         size = size + 1;
         int idx = 0;
-        //ensure size is less than capacity
-        if(size<capacity){
-            idx=size;
+        // ensure size is less than capacity
+        if (size < capacity) {
+            idx = size;
             arr[idx] = data;
         }
-        //fix the heap
-        while(idx>1 && arr[idx]>arr[idx/2] ){
-            swap(arr[idx],arr[idx/2]);
+        // fix the heap
+        while (idx > 1 && arr[idx] > arr[idx / 2]) {
+            swap(arr[idx], arr[idx / 2]);
         }
     }
 
