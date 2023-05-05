26f922469e7b0c2a38160754c511e3fffecfb362
kaushik.bora <herock76@gmail.com>
2023-05-05 20:49:34 +0530

    -input and format change

--- AdvancedModule/src/Searching/FindPivot.java
+++ AdvancedModule/src/Searching/FindPivot.java
@@ -2,29 +2,30 @@ package Searching;
 
 public class FindPivot {
     //given a sorted array, rotated kth times given k<len(A), find out k?
-    public static int findingPivot(int[] A){
-        int ans=0;
+    public static int findingPivot(int[] A) {
+        int ans = 0;
         int N = A.length;
         int start = 0;
-        int end = N-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(mid<end && A[mid]>A[mid-1] && A[mid]>A[mid+1]){
-                ans = mid+1;
+        int end = N - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
+                ans = mid + 1;
             }
-            if(mid>start && A[mid]<A[mid+1] && A[mid]<A[mid-1]){
+            if (mid > start && A[mid] < A[mid + 1] && A[mid] < A[mid - 1]) {
                 ans = mid;
             }
-            if(A[mid]>A[start]){
+            if (A[mid] > A[start]) {
                 start = mid + 1;
-            }else {
+            } else {
                 end = mid - 1; //if(A[mid]<A[start])
             }
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int[] A={4,5,6,7,0,1,2};
+        int[] A = {3, 4, 5, 6, 7, 0, 1, 2, 3};
         System.out.println(findingPivot(A));
     }
 }
