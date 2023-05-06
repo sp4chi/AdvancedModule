684381833d50453e820d286980fc1978fa50f0b2
kaushik.bora <herock76@gmail.com>
2023-05-06 06:10:24 +0530

    -format changes

--- AdvancedModule/src/ArrayScaler/ReverseDiagonals.java
+++ AdvancedModule/src/ArrayScaler/ReverseDiagonals.java
@@ -1,22 +1,23 @@
 package ArrayScaler;
 
 public class ReverseDiagonals {
-    public static void reverseDiagonals(int[][] A){
+    public static void reverseDiagonals(int[][] A) {
         int N = A.length;
-        for(int i=0;i<N;i++){
+        for (int i = 0; i < N; i++) {
             int row = i;
-            int col = N-1;
-            while(row>=0 && col>=0){
-                System.out.print(A[row][col]+" ");
-                row--;col--;
+            int col = N - 1;
+            while (row >= 0 && col >= 0) {
+                System.out.print(A[row][col] + " ");
+                row--;
+                col--;
             }
             System.out.println();
         }
-        for(int i=N-2;i>=0;i--){
-            int row = N-1;
-            int col = i ;
-            while(row>=0 && col>=0){
-                System.out.print(A[row][col]+" ");
+        for (int i = N - 2; i >= 0; i--) {
+            int row = N - 1;
+            int col = i;
+            while (row >= 0 && col >= 0) {
+                System.out.print(A[row][col] + " ");
                 row--;
                 col--;
             }
@@ -25,7 +26,7 @@ public class ReverseDiagonals {
     }
 
     public static void main(String[] args) {
-        int[][] A={
+        int[][] A = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
