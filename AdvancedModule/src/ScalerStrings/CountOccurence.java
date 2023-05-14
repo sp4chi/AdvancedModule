ffec5d5ddcc5fc449b6622058b484a6c6518fd20
kaushik.bora <herock76@gmail.com>
2023-05-15 00:56:48 +0530

    -added CountOccurence

---
+++ AdvancedModule/src/ScalerStrings/CountOccurence.java
@@ -0,0 +1,27 @@
+package ScalerStrings;
+
+public class CountOccurence {
+    //count ouccurences of "bob" in given String
+    public static int occurence(String A){
+        int N = A.length();
+        int cnt = 0;
+        int i = 0 ;
+        while(i<N){
+            if(A.contains("bob")){
+                cnt++;
+                int left = A.indexOf("bob");
+                int right = left+1;
+                String temp = A.substring(right);
+                A = temp;
+            }else{
+                i++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        String A = "bobob";
+        System.out.println(occurence(A));
+    }
+}
