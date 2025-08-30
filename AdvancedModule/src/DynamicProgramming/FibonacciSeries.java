0d020396dd70dd958af1877a1cba6c47abf4b2c0
sp4chi <herock76@gmail.com>
2025-08-31 04:23:31 +0530

    fib without cache

--- AdvancedModule/src/DynamicProgramming/FibonacciSeries.java
+++ AdvancedModule/src/DynamicProgramming/FibonacciSeries.java
@@ -1,6 +1,7 @@
 package DynamicProgramming;
 
 public class FibonacciSeries {
+    // top down
     public static int fib_rec(int N, int[] dp) {
         if (N == 0 || N == 1) {
             return N;
@@ -15,12 +16,31 @@ public class FibonacciSeries {
         dp[N] = f1 + f2;
         return dp[N];
     }
+
+    // bottom up
     public static int fib_loop(int N, int[] dp){
         for(int i=2;i<=N;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[N];
     }
+    //
+    public static int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+
+        int past = 0;   // Fib(0)
+        int prev = 1;   // Fib(1)
+        int curr = 0;
+
+        for (int i = 2; i <= N; i++) {
+            curr = past + prev;  // Fib(i)
+            past = prev;
+            prev = curr;
+        }
+        return curr;
+    }
+
     public static void main(String[] args) {
         int N = 6;
 
@@ -30,6 +50,6 @@ public class FibonacciSeries {
         dp[0] = 0;
         dp[1] = 1;
 
-        System.out.println(fib_loop(N, dp));
+        System.out.println(fib(N));
     }
 }
